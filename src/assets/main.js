f9877aff6f22494ba47da788de62df74fd1b851c
Paul McAleer <paul.mcaleer@liberty-it.co.uk>
2017-10-27 09:16:13 -0400

    Initial commit

--- src/assets/main.js
+++ src/assets/main.js
@@ -6,4 +6,11 @@ function guess() {
     //add functionality to guess function here
 }
 
-//implement new functions here
+function setHiddenFields(){
+    var randomNumber = Math.floor(Math.random() * 10000);
+    var stringNumber = randomNumber.toString();
+    while(stringNumber.length < 4){
+       stringNumber = "0" + stringNumber;
+    }
+    answer = stringNumber;
+};
