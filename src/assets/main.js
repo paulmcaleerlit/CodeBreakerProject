6f8e34ad4c33092628121a6beb278096a3ee8e8a
Paul McAleer <paul.mcaleer@liberty-it.co.uk>
2017-10-30 10:13:29 -0400

    Adding getResults function

--- src/assets/main.js
+++ src/assets/main.js
@@ -36,3 +36,32 @@ function validateInput(input){
   setMessage("Guesses must be exactly 4 characters long.");
   return false;
 }
+
+function getResults(input){
+  let results = document.getElementById('results');
+  var output = results.innerHTML;
+  output += '<div class="row"><span class="col-md-6">' + input + '</span><div class="col-md-6">';
+  for(var i=0;i<input.length;i++){
+    if(input.charAt(i) == answer.value.charAt(i)){
+      // Correct Position
+      output += '<span class="glyphicon glyphicon-ok"></span>';
+    } else if (charInString(input.charAt(i), answer.value)){
+      // Wrong Position
+      output += '<span class="glyphicon glyphicon-transfer"></span>';
+    } else {
+      // Not in the answer
+      output += '<span class="glyphicon glyphicon-remove"></span>';
+    }
+  }
+  output+= '</div></div>';
+  results.innerHTML = output;
+}
+
+function charInString(char, string){
+  for(var i=0;i<string.length;i++){
+    if(string.charAt(i) == char){
+      return true;
+    }
+  }
+  return false;
+}
